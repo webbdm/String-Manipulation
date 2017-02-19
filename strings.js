1d41e1d8d6a63f22099c23db879cc5ec6cb29e55
webbdm <gwebb762@gmail.com>
2017-02-18 22:19:25 -0600

    cleaned up the variable names and function order

--- strings.js
+++ strings.js
@@ -1,51 +1,45 @@
-var testString = "";
+var testString = "abc";
 
 
 var element = document.getElementById('text');
-
-var p = document.getElementById('p').innerHTML;
-
+var p = document.getElementById('p');
 var button = document.getElementById('button');
 
-// listener
-button.addEventListener("click", stringFunc);
 
-
-// input: string
-// returns: string
 function reversal(input) {
 	var output = input.split('').reverse().join('');
 	return output;
 }
 
-// input: string
-// output: string with letters sorted by alpha
+
 function alphabits(input) {
 	var output = input.split('').sort().join('');
 	return output;
 }
 
-// input: string
-// output: boolean indicating if the input is a palindrome
+
 function palindrome(input) {
-	var output = input === reversal(input);
+	var output = (input === reversal(input));
 	return output;
 }
 
 
-// Calls
+function write(input) {
+	p.innerHTML += '<p>'+input+'</p>';
+}
 
-function stringFunc() {
 
-	var input = element.value;
-	console.log(input);
-
-	reversal(input);
-	alphabits(input);
-	palindrome(input);
-
+function stringFunc(value) {
+	write(value);
+	write(reversal(value));
+	write(alphabits(value));
+	write(palindrome(value));
 };
 
+// listener
+button.addEventListener("click", function() {
+	stringFunc(element.value);
+});
 
 
 
